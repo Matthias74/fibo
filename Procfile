1576dd5b32182558746629709bb1d2afe38fb3a5
Matthias Vitte <vittematthias@gmail.com>
2017-05-26 17:25:53 +0200

    changing the procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python fibo.py 10
+bot: python fibo.py
--- fibo.py
+++ fibo.py
@@ -15,3 +15,5 @@ def fib2(n):   # return Fibonacci series up to n
         result.append(b)
         a, b = b, a+b
     return result
+
+fib(3)
