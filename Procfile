eb0819869c062bd12fe397a84ae6b8202e32f0a5
Matthias Vitte <vittematthias@gmail.com>
2017-05-26 17:03:14 +0200

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python fibo.py 10
+web: python fibo.py
--- fibo.py
+++ fibo.py
@@ -2,16 +2,16 @@
 
 # Fibonacci numbers module
 
-
-def fib(n):    # write Fibonacci series up to n
-
-        print "Bonjour", n
-
-
-def fib2(n):   # return Fibonacci series up to n
-    result = []
-    a, b = 0, 1
-    while b < n:
-        result.append(b)
-        a, b = b, a+b
-    return result
+print "hello Matt"
+# def fib(n):    # write Fibonacci series up to n
+#
+#         print "Bonjour", n
+#
+#
+# def fib2(n):   # return Fibonacci series up to n
+#     result = []
+#     a, b = 0, 1
+#     while b < n:
+#         result.append(b)
+#         a, b = b, a+b
+#     return result
