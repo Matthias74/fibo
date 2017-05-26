6eeb797fcc495d4c4705d4efc1313b710693df4d
Matthias Vitte <vittematthias@gmail.com>
2017-05-26 16:42:00 +0200

    correcting the script

--- fibo.py
+++ fibo.py
@@ -13,6 +13,6 @@ def fib2(n):   # return Fibonacci series up to n
         a, b = b, a+b
     return result
 
-if __name__ == "__main__":
-    import sys
-    fib(int(sys.argv[1]))
+# if __name__ == "__main__":
+#     import sys
+#     fib(int(sys.argv[1]))
